102d9bf4de30e75e78d0b00981113f326ef47228
iprih <priscila.silva076@gmail.com>
2020-02-04 21:20:25 -0300

    adicionando aulas

---
+++ Gama.Calculadora/Console/Calculadora.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gama.Calculadora.Console
+{
+    public static class Calculadora
+    {
+        public static void Calcular()
+        {
+            while (true)
+            {
+
+                // mensagens no console
+                System.Console.WriteLine("Escola a operação:");
+                System.Console.WriteLine("1 - DIVISÃO");
+                System.Console.WriteLine("2 - MULTIPLICAÇÃO");
+                System.Console.WriteLine("3 - SOMA");
+                System.Console.WriteLine("4 - SUBTRAÇÃO");
+                System.Console.WriteLine("Digite o numero:");
+
+                // declarações
+                var op = System.Console.ReadLine();
+                var a = 2;
+                var b = 3;
+
+                switch (op)
+                {
+                    case "1":
+                        System.Console.WriteLine(Div(a, b));
+                        break;
+
+                    case "2":
+                        System.Console.WriteLine(Multi(a, b));
+                        break;
+
+                    case "3":
+                        System.Console.WriteLine(Soma(a, b));
+                        break;
+
+                    case "4":
+                        System.Console.WriteLine(Sub(a, b));
+                        break;
+
+                    default:
+                        System.Console.WriteLine("Opção inválida");
+                        break;
+                }
+
+                System.Console.ReadKey();
+                System.Console.Clear();
+            }
+        }
+        static int Soma(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int Multi(int a, int b)
+        {
+            return a * b;
+        }
+
+        static double Div(int a, int b)
+        {
+            return (double)a / b;
+        }
+    }
+
+}
---
+++ Gama.Calculadora/Console/Colecoes.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq; //
+
+namespace Gama.Calculadora.Console
+{
+    public class Colecoes
+    {
+        /// <summary>
+        /// Arrays são coleções não dinamicas, ou seja, não se adiciona em tempo de execução!
+        /// </summary>
+        public static void TrabalhandoComArray()
+        {
+            var nomes = new string[5]; 
+            //forma de declarar array
+            nomes[0] = "yoda";
+            nomes[1] = "pri";
+            nomes[2] = "pimpa";
+            nomes[3] = "pipoca";
+            nomes[4] = "iris";
+
+            
+            for (int i = 0; i < nomes.Length ; i++)
+            {
+                System.Console.WriteLine(nomes[i]);
+            }
+        }
+
+        /// <summary>
+        /// Lista é dinamica
+        /// </summary>
+        public static void TrabalhandoComLista()
+        {
+            var nomes = new List<string>() 
+            {
+                "Priscila",
+                "Pimpa"
+            };
+
+            while (true)
+            {
+                foreach (var n in nomes)
+                {
+                    System.Console.WriteLine(n);
+                }
+
+                System.Console.WriteLine("Informe o nome: ");
+                
+                var novo = System.Console.ReadLine();
+                
+                nomes.Add(novo);
+                
+                System.Console.Clear();
+            }
+        }
+        /// <summary>
+        /// FIFO - Nao da pra remover alguem do meio da lista
+        /// </summary>
+        public static void TrabalhandoComFila()
+        {
+            var nomes = new Queue<string>();
+            nomes.Enqueue("Joana");
+            nomes.Enqueue("Maria");
+            nomes.Enqueue("Joaquina");
+
+            while (true)
+            {
+                foreach (var n in nomes)
+                {
+                    System.Console.WriteLine(n);
+                }
+
+                System.Console.WriteLine("Pressione qualquer tecla para retirar da fila: ");
+
+                System.Console.ReadKey();
+
+                nomes.Dequeue();
+
+                System.Console.Clear();
+            }
+        }
+
+        public static void TrabalhandoComPilha()
+        {
+            var nomes = new Stack<string>();
+            nomes.Push("Joana");
+            nomes.Push("Maria");
+            nomes.Push("Joaquina");
+
+            while (true)
+            {
+                foreach (var n in nomes)
+                {
+                    System.Console.WriteLine(n);
+                }
+
+                System.Console.WriteLine("Pressione qualquer tecla para retirar da fila: ");
+
+                System.Console.ReadKey();
+
+                nomes.Pop();
+
+                System.Console.Clear();
+            }
+        }
+
+        public static void TrabalhandoComDicionario()
+        {
+            var nomes = new Dictionary<string, int>();
+                nomes.Add("Joana", 22);
+                nomes.Add("Amanda", 32);
+                nomes.Add("Bruna", 13);
+
+            var resultado = nomes.First(item => item.Key == "Amanda");
+            var resultado2 = nomes.First(item => item.Value == 22);
+
+            //foreach(var n in nomes)
+
+            System.Console.WriteLine(resultado);
+            System.Console.WriteLine(resultado2);
+
+        }
+
+        public static void FiltrandoLista()
+        {
+            var nomes = new List<string>()
+            {
+                "Priscila",
+                "Pimpa"
+            };
+            var resultado = nomes.SingleOrDefault(item => item == "Priscila");
+            //var resultado = nomes.Single(item => item == "Priscila");
+
+            System.Console.WriteLine(resultado);
+
+        }
+
+    }
+}
