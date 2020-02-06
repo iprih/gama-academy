b1c9ba0946ad5e16b8409f1282719000148dbdf6
iprih <priscila.silva076@gmail.com>
2020-02-05 21:34:52 -0300

    adicionando projeto base

---
+++ Gama.RedeSocial/src/Domain/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gama.RedeSocial.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public bool Active { get; set; }
+    }
+}
--- Gama.RedeSocial/src/Domain/Entities/User.cs
+++ Gama.RedeSocial/src/Domain/Entities/User.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Gama.RedeSocial.Domain.Entities
-{//atalho prop - ou ctor 
-    class User
+{
+    class User : BaseEntity
     {
         public string Name { get; set; }//get - ler e set - definir
         public string Email { get; set; }
         public string Password { get; set; }
         public DateTime Birthday { get; set; }
-        public string Gender { get; set; }
-
-        
+        public string Gender { get; set; }   
 
     }
 }
